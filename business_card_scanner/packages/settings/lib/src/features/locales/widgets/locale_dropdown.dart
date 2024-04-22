2ca549ebb47068a3a6cd0bef63ce2413508b8ad5
Endrju00 <andrzej.kapczynski21@gmail.com>
2024-04-22 22:56:52 +0200

    feat(settings): Update language dropdown

--- business_card_scanner/packages/settings/lib/src/features/locales/widgets/locale_dropdown.dart
+++ business_card_scanner/packages/settings/lib/src/features/locales/widgets/locale_dropdown.dart
@@ -1,27 +1,39 @@
+import 'package:core/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:settings/src/features/locales/cubit/locale_cubit.dart';
 
 class LocaleDropdown extends StatelessWidget {
   const LocaleDropdown({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
-      elevation: 0,
-      borderRadius: BorderRadius.circular(16),
-      alignment: Alignment.centerLeft,
-      underline: Container(),
-      value: const Locale('en'),
-      items: const [
-        DropdownMenuItem(
-          value: Locale('en'),
-          child: Text('English'),
-        ),
-        DropdownMenuItem(
-          value: Locale('pl'),
-          child: Text('Polish'),
-        ),
-      ],
-      onChanged: (value) {},
+    return BlocBuilder<LocaleCubit, LocaleState>(
+      builder: (context, state) {
+        if (state is LocaleSaving) {
+          return const CircularProgressIndicator.adaptive();
+        }
+
+        return DropdownButton(
+          elevation: 0,
+          borderRadius: BorderRadius.circular(16),
+          alignment: Alignment.centerLeft,
+          underline: Container(),
+          value: state is LocaleLoaded ? state.locale : const Locale('en'),
+          items: const [
+            DropdownMenuItem(
+              value: Locale('en'),
+              child: Text('English'),
+            ),
+            DropdownMenuItem(
+              value: Locale('pl'),
+              child: Text('Polish'),
+            ),
+          ],
+          onChanged: (value) async {
+            await context.read<LocaleCubit>().cacheLocale(value!);
+          },
+        );
+      },
     );
   }
 }
