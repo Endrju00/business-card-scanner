b5acdb8dc1fdb97941fce09c7ffa0c37a4339aaa
Endrju00 <andrzej.kapczynski21@gmail.com>
2024-05-05 18:54:36 +0200

    fix: Move app drawer to separate file

--- business_card_scanner/lib/src/presentation/pages/home_page.dart
+++ business_card_scanner/lib/src/presentation/pages/home_page.dart
@@ -1,20 +1,12 @@
-import 'package:core/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:settings/settings.dart';
 
+import '../widgets/app_drawer.dart';
 import '../widgets/bcs_container.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void _openBottomSheet(BuildContext context) => showModalBottomSheet<void>(
-        context: context,
-        isDismissible: false,
-        useSafeArea: true,
-        builder: (BuildContext context) => const SettingsSheet(),
-      );
 
   @override
   Widget build(BuildContext context) {
@@ -37,35 +29,7 @@ class HomePage extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
       ),
-      endDrawer: Drawer(
-        backgroundColor: Colors.white,
-        child: SafeArea(
-          child: Column(
-            children: [
-              Opacity(
-                opacity: 0.7,
-                child: SvgPicture.asset(
-                  'assets/logo.svg',
-                  width: MediaQuery.of(context).size.width * 0.5,
-                ),
-              ),
-              Text(
-                AppLocalizations.of(context)!.appName,
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const Spacer(),
-              BcsButton(
-                onPressed: () => _openBottomSheet(context),
-                text: AppLocalizations.of(context)!.settings,
-              ),
-              BcsButton(
-                onPressed: () {},
-                text: AppLocalizations.of(context)!.logOut,
-              ),
-            ],
-          ),
-        ),
-      ),
+      endDrawer: const AppDrawer(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
